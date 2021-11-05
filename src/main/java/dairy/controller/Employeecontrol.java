2c92454c44d187507b627c3cf091919667d028ff
zilzaak <orchidpust@gmail.com>
2021-11-05 19:58:11 +0600

    omar

---
+++ src/main/java/dairy/controller/Employeecontrol.java
@@ -0,0 +1,152 @@
+package dairy.controller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import dairy.model.Kormi;
+import dairy.repo.Employeerepo;
+
+
+@Controller
+@RequestMapping("/employee")
+public class Employeecontrol {
+	@Autowired
+	private Employeerepo err;
+	
+	@PostMapping("/addemp")
+public ResponseEntity<Kormi> addemployee(@RequestBody Kormi emp){
+		boolean ed=false;
+		boolean enid=false;
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			Date d = sdf.parse(emp.getStringjoindate());
+			emp.setJoindate(d);
+		} catch (ParseException e) {
+			ed=true;
+			e.printStackTrace();
+		}
+		if(err.existsByNid(emp.getNid())) {
+			enid=true;
+		}
+		
+		
+		if(ed || enid) {
+			emp.setAddress("invalid date or NID already exist");
+			return new  ResponseEntity<Kormi>(emp,HttpStatus.OK);
+		}
+		
+		else{
+	String s = Integer.toString((int) emp.getSalary());
+	emp.setStringsalary(s);
+	emp.setActivestatus("active worker");
+		err.save(emp);	
+		emp.setAddress("successfully added employee");
+		return new  ResponseEntity<Kormi>(emp,HttpStatus.OK);
+		
+		}
+	
+		}
+	
+
+	
+	@PostMapping("/searchemp")
+public ResponseEntity<List<Kormi>> empsearch(@RequestBody Kormi emp){
+		List<Kormi> lst =new ArrayList<Kormi>();
+	if(emp.getNid().contentEquals("name")) {
+		lst=err.findByNameContaining(emp.getName());
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	
+	if(emp.getNid().contentEquals("phone")) {
+		lst=err.findByPhoneContaining(emp.getPhone());
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	
+	if(emp.getNid().contentEquals("address")) {
+		lst=err.findByAddressContaining(emp.getAddress());
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	
+	if(emp.getNid().contentEquals("left")) {
+		lst=err.findByActivestatus("left job");
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	
+	if(emp.getNid().contentEquals("active")) {
+		lst=err.findByActivestatus("active worker");
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	if(emp.getNid().contentEquals("all")) {
+		lst=err.findAll();
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	if(emp.getNid().contentEquals("salary")) {
+		lst=err.findByStringsalaryContaining(emp.getStringsalary());
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	
+	
+	if(emp.getNid().contentEquals("designation")) {
+		lst=err.findByDesignationContaining(emp.getDesignation());
+		return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	}
+	
+	return new ResponseEntity<List<Kormi>>(lst,HttpStatus.OK);
+	
+		}	
+	
+	@RequestMapping(value="/editemp",method=RequestMethod.PUT)
+public ResponseEntity<Kormi> editemp(@RequestBody Kormi emp){
+		boolean ed=false;
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			Date d = sdf.parse(emp.getStringjoindate());
+			emp.setJoindate(d);
+		} catch (ParseException e) {
+			ed=true;
+			e.printStackTrace();
+		}
+
+		
+		if(ed) {
+			emp.setAddress("invalid date or NID already exist");
+			return new  ResponseEntity<Kormi>(emp,HttpStatus.OK);
+		}
+		
+		else{
+			if(emp.getActivestatus().contentEquals("left job")) {
+			String d = sdf.format(new Date());
+			emp.setLeftdate(d);
+			}
+	String s = Integer.toString((int) emp.getSalary());
+	emp.setStringsalary(s);
+		err.save(emp);	
+		emp.setAddress("employee edited successfully");
+		return new  ResponseEntity<Kormi>(emp,HttpStatus.OK);
+		
+		}
+	
+		}
+	
+	@DeleteMapping("/deleteemp")
+public ResponseEntity<Kormi> deleteemp(@RequestBody Kormi emp){
+	err.delete(emp);
+	
+	return new  ResponseEntity<Kormi>(emp,HttpStatus.OK);
+		}	
+
+}
+
